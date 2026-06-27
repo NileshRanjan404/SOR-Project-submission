8e25c829073e6bb306e4e8c8e6a1d28e06546c0c
Sachin <sachinmandal3580@gmail.com>
2026-06-27 08:20:13 +0530

    pre-final

--- build/rover_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/sachin/ros2_rover/install/rover_msgs/share/rover_msgs/package.sh"
-. "/home/sachin/ros2_rover/install/rover_motor_controller_cpp/share/rover_motor_controller_cpp/package.sh"
